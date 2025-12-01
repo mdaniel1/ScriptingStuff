5f9f54400a2db96582973fc3ef3ce4a2076719cc
M. Daniel <46349079+mdaniel1@users.noreply.github.com>
2025-12-01 21:18:31 +0100

    Added all EV items to the cheats + removed clutter comments

--- boot_patch.rb
+++ boot_patch.rb
@@ -2,313 +2,6 @@
 puts "[INJECT] boot_patch loaded"
 
 # --------------------------------------------------
-#  Dumper (safe: no Set, no external deps)
-# --------------------------------------------------
-#module PSDKSaveDump
-#  SAVE_DUMP_PATH = "save_dump.txt"
-#
-#  def self.extract_gold(trainer)
-#    if defined?($game_party) && $game_party
-#      return $game_party.respond_to?(:gold) ? $game_party.gold : $game_party.instance_variable_get(:@gold)
-#    end
-#
-#    gs = if defined?(PFM) && PFM.respond_to?(:game_state)
-#          PFM.game_state
-#        elsif defined?($game_state)
-#          $game_state
-#        else
-#          trainer.instance_variable_get(:@game_state) rescue nil
-#        end
-#
-#    if gs && gs.instance_variables.include?(:@game_party)
-#      gp = gs.instance_variable_get(:@game_party)
-#      return gp.respond_to?(:gold) ? gp.gold : gp.instance_variable_get(:@gold)
-#    end
-#
-#    if trainer.respond_to?(:actual_money)
-#      return trainer.actual_money
-#    end
-#
-#    nil
-#  end
-#
-#  def self.dump(game_state)
-#    File.open(SAVE_DUMP_PATH, "w:utf-8") do |f|
-#      f.puts "--- SAVE DUMP START ---"
-#
-#      begin
-#        trainer = game_state.trainer rescue nil
-#        gp = $game_party
-#        gp.instance_variables.each { |iv| f.puts "#{iv} = #{gp.instance_variable_get(iv).inspect}" }
-#        game_state = PFM.game_state
-#        bag = game_state.instance_variable_get(:@bag)
-#
-#        f.puts "DEBUG Input: #{defined?(Input)}"
-#        f.puts "DEBUG Yuki::Keyboard: #{defined?(Yuki::Keyboard) rescue false}"
-#        f.puts "DEBUG LiteRGSS: #{defined?(LiteRGSS) rescue false}"
-#        File.open("litergss_dump.txt", "w:utf-8") do |ff|
-#          ff.puts "=== LiteRGSS constants ==="
-#          ff.puts(LiteRGSS.constants.inspect) rescue nil
-#
-#          ff.puts "\n=== LiteRGSS methods ==="
-#          ff.puts(LiteRGSS.methods.sort.inspect) rescue nil
-#
-#          ff.puts "\n=== LiteRGSS singleton methods ==="
-#          ff.puts(LiteRGSS.singleton_methods.sort.inspect) rescue nil
-#
-#          LiteRGSS.constants.each do |const|
-#            begin
-#              klass = LiteRGSS.const_get(const)
-#              ff.puts "\n--- #{const} ---"
-#              ff.puts "class: #{klass}"
-#              ff.puts "methods: #{klass.methods(false).inspect}"
-#              ff.puts "instance_methods: #{klass.instance_methods(false).inspect}"
-#            rescue => e
-#              ff.puts "\n--- #{const} ---"
-#              ff.puts "ERROR: #{e}"
-#            end
-#          end
-#        end
-#
-#        LiteRGSS.constants.each do |const|
-#          begin
-#            klass = LiteRGSS.const_get(const)
-#            f.puts "\n--- #{const} ---"
-#            f.puts "class: #{klass}"
-#            f.puts "methods: #{klass.methods(false).inspect rescue nil}"
-#            f.puts "instance_methods: #{klass.instance_methods(false).inspect rescue nil}"
-#          rescue => e
-#            f.puts "\n--- #{const} ---"
-#            f.puts "ERROR: #{e}"
-#          end
-#        end
-#
-#        if gp
-#          f.puts "== Game_Party =="
-#          gp.instance_variables.each do |iv|
-#            begin
-#              val = gp.instance_variable_get(iv)
-#              f.puts "  #{iv} = #{val.inspect}"
-#            rescue => e
-#              f.puts "  #{iv} = <error #{e.message}>"
-#            end
-#          end
-#        else
-#          f.puts "== Game_Party not found =="
-#        end
-#
-#        if bag
-#          f.puts "== Bag =="
-#          bag.instance_variables.each do |iv|
-#            begin
-#              val = bag.instance_variable_get(iv)
-#              f.puts "  #{iv} = #{val.inspect}"
-#            rescue => e
-#              f.puts "  #{iv} = <error #{e.message}>"
-#            end
-#          end
-#        else
-#          f.puts "== Bag not found =="
-#        end
-#
-#        f.puts "=== BAG POCKET STRUCTURE ==="
-#        begin
-#          if bag
-#            orders = bag.instance_variable_get(:@orders)
-#            items  = bag.instance_variable_get(:@items)
-#
-#            f.puts "Number of pockets: #{orders.length rescue '???'}"
-#
-#            # Dump content of each pocket
-#            orders.each_with_index do |pocket, idx|
-#              f.puts "\n-- Pocket #{idx} --"
-#              if pocket.is_a?(Array)
-#                pocket.each do |sym|
-#                  qty = items[sym] || 0
-#                  f.puts "  #{sym.inspect} => #{qty}"
-#                end
-#              else
-#                f.puts "  (invalid pocket: #{pocket.inspect})"
-#              end
-#            end
-#
-#            if defined?(GameData) && GameData.const_defined?(:Item)
-#              f.puts "\n=== ITEM → POCKET MAP ==="
-#              GameData::Item.all.each do |item|
-#                begin
-#                  sym = item.id
-#                  pocket = bag.pocket_id(sym) rescue "ERR"
-#                  f.puts "  #{sym.to_s.ljust(20)} pocket=#{pocket}"
-#                rescue => e
-#                  f.puts "  ERROR reading item #{item.inspect}: #{e}"
-#                end
-#              end
-#            else
-#              f.puts "GameData::Item not available!"
-#            end
-#
-#          else
-#            f.puts "Bag not found!"
-#          end
-#
-#        rescue => e
-#          f.puts "ERROR dumping pockets: #{e.class}: #{e.message}"
-#        end
-#
-#        f.puts "=== END BAG POCKET STRUCTURE ==="
-#
-#        sys = game_state.instance_variable_get(:@system)
-#        f.puts "(DEBUG) sys : #{sys}"
-#        if sys
-#          f.puts "== Game_System =="
-#          sys.instance_variables.each do |iv|
-#            f.puts "  #{iv} = #{sys.instance_variable_get(iv).inspect}"
-#          end
-#        end
-#
-#        if trainer
-#          f.puts "== Trainer ==: #{trainer.instance_variables.inspect}"
-#          trainer.instance_variables.each do |ivar|
-#            begin
-#              val = trainer.instance_variable_get(ivar)
-#              f.puts "  #{ivar} = #{val.inspect}"
-#            rescue => e
-#              f.puts "  #{ivar} = <error #{e.message}>"
-#            end
-#          end
-#
-#          name =
-#            if trainer.respond_to?(:actual_name)
-#              trainer.actual_name
-#            elsif trainer.respond_to?(:name)
-#              trainer.name
-#            else
-#              "(no name)"
-#            end
-#
-#          f.puts "Trainer: #{name}"
-#          gold = extract_gold(trainer)
-#          f.puts "Money: #{gold}"
-#
-#          
-#        else
-#          f.puts "Trainer: not found"
-#        end
-#
-#        party = game_state.actors rescue nil
-#        if party.respond_to?(:each)
-#          f.puts "Party:"
-#          party.each_with_index do |p, i|
-#            next unless p
-#
-#            # Name
-#            pname =
-#              if p.respond_to?(:given_name) && p.given_name
-#                p.given_name
-#              elsif p.respond_to?(:name)
-#                p.name
-#              elsif p.respond_to?(:db_symbol)
-#                p.db_symbol.to_s
-#              else
-#                "???"
-#              end
-#
-#            # Level
-#            level = p.respond_to?(:level) ? p.level : "?"
-#
-#            # IVs (PSDK naming convention)
-#            begin
-#              iv_hp  = p.iv_hp  rescue "?"
-#              iv_atk = p.iv_atk rescue "?"
-#              iv_def = p.iv_dfe rescue "?"
-#              iv_spa = p.iv_ats rescue "?"
-#              iv_spd = p.iv_dfs rescue "?"
-#              iv_spe = p.iv_spd rescue "?"
-#            rescue
-#              iv_hp = iv_atk = iv_def = iv_spa = iv_spd = iv_spe = "?"
-#            end
-#
-#            f.puts "  [#{i}] #{pname} Lv#{level} | IVs: (hp: #{iv_hp}, atk: #{iv_atk}, def: #{iv_def}, spa: #{iv_spa}, spd: #{iv_spd}, spe: #{iv_spe})"
-#          end
-#        else
-#          f.puts "Party: not found"
-#        end
-#
-#      rescue => e
-#        f.puts "ERROR: #{e.class}: #{e.message}"
-#      end
-#
-#      f.puts "=== INPUT SYSTEM DUMP ==="
-#
-#      begin
-#        if defined?(Input::Keys)
-#          f.puts "-- Input::Keys --"
-#          Input::Keys.each do |sym, arr|
-#            f.puts "  #{sym.inspect} => #{arr.inspect}"
-#          end
-#        else
-#          f.puts "Input::Keys NOT FOUND"
-#        end
-#
-#        if defined?(Input::ALIAS_KEYS)
-#          f.puts "-- Input::ALIAS_KEYS --"
-#          Input::ALIAS_KEYS.each do |keycode, action|
-#            f.puts "  #{keycode} => #{action.inspect}"
-#          end
-#        else
-#          f.puts "Input::ALIAS_KEYS NOT FOUND"
-#        end
-#
-#        f.puts "-- Current pressed Input Keys --"
-#        if defined?(Input)
-#          Input::Keys.keys.each do |sym|
-#            begin
-#              if Input.press?(sym)
-#                f.puts "  #{sym.inspect} => PRESSED"
-#              end
-#            rescue => e
-#            end
-#          end
-#        end
-#
-#        if defined?(Sf) && defined?(Sf::Keyboard)
-#          f.puts "-- SFML Keycodes --"
-#          Sf::Keyboard.constants.each do |const|
-#            val = Sf::Keyboard.const_get(const) rescue nil
-#            next unless val.is_a?(Integer)
-#            f.puts "  #{const} = #{val}"
-#          end
-#        end
-#
-#      rescue => e
-#        f.puts "ERROR IN INPUT DUMP: #{e.class}: #{e.message}"
-#      end
-#
-#      f.puts "=== END INPUT SYSTEM DUMP ==="
-#      #File.open("pfm_wild_methods.txt", "w") do |fff|
-#      if defined?(PFM::Wild_Battle)
-#          f.puts PFM::Wild_Battle.methods(false)
-#          f.puts PFM::Wild_Battle.singleton_methods(false)
-#          f.puts PFM::Wild_Battle.instance_methods(false)
-#        else
-#          f.puts "NO Wild_Battle"
-#        end
-#      #end
-#      if defined?(PFM) && defined?(PFM::Pokemon)
-#        File.open("pokemon_methods.txt", "w") do |f|
-#          f.puts "=== METHODS OF PFM::Pokemon ==="
-#          PFM::Pokemon.instance_methods(false).each do |m|
-#            f.puts m
-#          end
-#        end
-#      end
-#
-#      f.puts "--- SAVE DUMP END ---"
-#    end
-#  end
-#end
-
-# --------------------------------------------------
 #  Hook manager (no Set)
 # --------------------------------------------------
 module SaveEditorHook
@@ -377,7 +70,7 @@ module SaveEditorHook
       end
 
       # === ADDING ITEMS ===
-      if PSDKCheat.consume_combo?(:L, :R) #Keyboard input : F and G
+      if PSDKCheat.consume_combo?(:L, :R) #Keyboard input : F + G
         puts "[CHEAT] Combo L+R detected"
         PSDKCheat.give_item(gs, :rare_candy, 10, 6)
       end
@@ -387,14 +80,24 @@ module SaveEditorHook
         PSDKCheat.give_item(gs, :link_stone, 1, 1) 
       end
 
-      if PSDKCheat.consume_combo?(:R, :START)
+      if PSDKCheat.consume_combo?(:R, :START) #Keyboard input : G + J
         puts "[CHEAT] Combo R+START detected -> Master Ball"
-        PSDKCheat.give_item(gs, :master_ball, 1, 2) #Keyboard input : G + J
+        PSDKCheat.give_item(gs, :master_ball, 1, 2) 
       end
 
-      if PSDKCheat.consume_combo?(:R, :R3)
+      if PSDKCheat.consume_combo?(:R, :R3) #Keyboard input : G + U
         puts "[CHEAT] Combo R+START detected -> PP Max"
-        PSDKCheat.give_item(gs, :pp_max, 10, 6) #Keyboard input : G + U
+        PSDKCheat.give_item(gs, :pp_max, 10, 6) 
+      end
+
+      if PSDKCheat.consume_combo?(:L, :R3) #Keyboard input : F + U
+        puts "[CHEAT] Combo L+START detected -> All EV items"
+        PSDKCheat.give_item(gs, :iron, 100, 6)
+        PSDKCheat.give_item(gs, :protein, 100, 6)
+        PSDKCheat.give_item(gs, :calcium, 100, 6)
+        PSDKCheat.give_item(gs, :zinc, 100, 6)
+        PSDKCheat.give_item(gs, :hp_up, 100, 6)
+        PSDKCheat.give_item(gs, :carbos, 100, 6)
       end
 
       # === FORCING SHINY ===
@@ -412,7 +115,6 @@ module SaveEditorHook
     rescue => e
       puts "[CHEAT DEBUG] Error: #{e}"
     end
-
   end
 
   def self.install_for(klass)
